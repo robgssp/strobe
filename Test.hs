09581d72e98b7648e015b3640f2b9c0c426e21a8
Robert Glossop <robgssp@gmail.com>
2015-06-19 13:27:15 -0400

    working perspective

--- Test.hs
+++ Test.hs
@@ -37,7 +37,7 @@ data Input
   | Joystick JoystickButtons JoystickPosition
   deriving (Eq,Show)
 
-type State = (Double,Double,Double)
+type State = (Float,Float,Float)
 
 main :: IO ()
 main =
@@ -50,7 +50,7 @@ main =
                      (\_ _ st ->
                         do writeIORef state st
                            return False)
-                     ctl
+                     ctl1
 
      let doReact st = 
            do t <- get oldTime
@@ -59,18 +59,17 @@ main =
               let td = fromIntegral (t' - t) / 1000
               printf "Feeding %s at %d\n" (show st) (t' - t)
               void $ react rh (td, st)
-         initWindow win = return ()
-           -- do keyboardMouseCallback $=
-           --           Just (\key kstate _ _ -> doReact (Just (Key key kstate)))
+         initWindow win =
+           do keyboardMouseCallback $=
+                     Just (\key kstate _ _ -> doReact (Just (Key key kstate)))
 
-           --    joystickCallback $=
-           --           Just (\buttons pos -> doReact (Just (Joystick buttons pos)), 10)
+              joystickCallback $=
+                     Just (\buttons pos -> doReact (Just (Joystick buttons pos)), 10)
 
-           --    idleCallback $= Just (postRedisplay Nothing)
+              idleCallback $= Just (postRedisplay Nothing)
+              perWindowKeyRepeat $= PerWindowKeyRepeatOff
 
      newWindow "test" (100:.100:.()) (800:.600:.()) (display state (doReact Nothing)) initWindow
-
-     perWindowKeyRepeat $= PerWindowKeyRepeatOff
 
      let keyval dir = if dir == Up then 1 else 0
 
@@ -79,24 +78,55 @@ main =
 display :: IORef State -> IO () -> Vec2 Int -> IO (FrameBuffer RGBFormat () ())
 display state step size = 
   do step
-     st <- readIORef state
-     return (frameBuffer (redFrag (trans (cube TriangleFan))))
+     (x,_,angle) <- readIORef state
+     printf "loc %f\n" x
+     return (frameBuffer (color (RGB (1:.0:.0:.())) 
+                                (frag (trans angle
+                                             x
+                                             size 
+                                             (cube TriangleFan)))
+                       <> color (RGB (0:.1:.0:.())) 
+                                (frag (trans angle
+                                             x
+                                             size 
+                                             (cube LineStrip)))
+                       <> color (RGB (0:.0:.1:.()))
+                                (frag (trans 0
+                                             0
+                                             size
+                                             (cube TriangleFan)))))
 
 cube :: Primitive p => p -> PrimitiveStream p (Vec3 (Vertex Float))
-cube p = mconcat (map ((toGPUStream p).(`map` ps)) [\(x,y) -> x:.y:.0:.()
-                                                   ,\(x,y) -> y:.x:.1:.()
+cube p = mconcat (map ((toGPUStream p).(`map` ps)) [\(x,y) -> x:.y:.1:.()
+                                                   ,\(x,y) -> y:.x:.(-1):.()
                                                    ])
-  where ps = [(0,0),(1,0),(1,1),(0,1)]
+  where ps = [(-1,-1),(1,-1),(1,1),(-1,1)]
 
 tri :: PrimitiveStream Triangle (Vec3 (Vertex Float))
 tri = toGPUStream TriangleStrip [1:.0:.0:.(), 0:.1:.0:.(), 0:.0:.0:.()]
 
-trans :: Primitive p => PrimitiveStream p (Vec3 (Vertex Float)) -> PrimitiveStream p (Vec4 (Vertex Float))
-trans = fmap trans'
-  where trans' pos = translation ((-0.5):.(-0.5):.0:.()) `multmv` homPoint pos
+trans :: Primitive p
+      => Float
+      -> Float
+      -> Vec2 Int
+      -> PrimitiveStream p (Vec3 (Vertex Float))
+      -> PrimitiveStream p (Vec4 (Vertex Float))
+trans angle x (w:.h:.()) = fmap trans'
+  where trans' pos = (toGPU (cameraMat `multmm` locMat) :: Mat44 (Vertex Float)) `multmv`
+                     homPoint pos
+        locMat = translation (0:.0:.x:.()) `multmm` 
+                 scaling (0.25:.0.25:.0.25:.()) `multmm` 
+                 rotationVec (normalize (1:.1:.1:.())) angle :: Mat44 Float
+        cameraMat = perspective 0.25 100 (pi/3) (fromIntegral w / fromIntegral h) `multmm`
+                    translation (0:.0:.(-3):.()) :: Mat44 Float
 
-redFrag :: PrimitiveStream p (Vec4 (Vertex Float)) -> FragmentStream (Color RGBFormat (Fragment Float))
-redFrag vs = const (RGB (1:.0:.0:.())) <$> rasterizeFront ((,()) <$> vs)
+frag :: PrimitiveStream p (Vec4 (Vertex Float)) -> FragmentStream ()
+frag vs = rasterizeFront ((,()) <$> vs)
+
+color :: (Color RGBFormat (Fragment Float))
+      -> FragmentStream a
+      -> FragmentStream (Color RGBFormat (Fragment Float))
+color = fmap . const
 
 frameBuffer :: FragmentStream (Color RGBFormat (Fragment Float)) -> FrameBuffer RGBFormat () ()
 frameBuffer fs = paintSolid fs emptyFrameBuffer
@@ -142,11 +172,28 @@ ctl =
           vel0 <- iPre (vector2 0 0) -< vel
         
           wall' <- iPre NoEvent -< wallX `merge` wallY
-          let bound = clampVec (-1) 1 (-1) 1 pos0
+          let bound = clampVec (- 1) 1 (- 1) 1 pos0
           pos <- impulseIntegral -< (vel, Event (bound ^-^ pos0))
           pos0 <- iPre (vector2 0 0) -< pos
 
         returnA -< (vector2X pos, vector2Y pos, angle)
+  where boolN True = 1
+        boolN False = 0
+        keyNum k = boolN ^<< keyIsDown k
+
+ctl1 :: SF Input State
+ctl1 = 
+  proc input -> do
+        up <- keyNum (SpecialKey KeyUp) -< input
+        down <- keyNum (SpecialKey KeyDown) -< input
+        left <- keyNum (SpecialKey KeyLeft) -< input
+        right <- keyNum (SpecialKey KeyRight) -< input
+        (btns,JoystickPosition jx jy jr) <- hold (undefined,JoystickPosition 0 0 0) -< joystickEvent input
+
+        x <- integral -< (up - down)
+        angle <- integral -< 2 * (left - right)
+
+        returnA -< (x*10-2, 0, angle)
   where boolN True = 1
         boolN False = 0
         keyNum k = boolN ^<< keyIsDown k
